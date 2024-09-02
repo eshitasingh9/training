b5628ed396dffe12b3f7ce5b09bf0a80a078c0f8
harshp1509 <hhpatel1509@gmail.com>
2024-09-02 10:57:43 +0000

    hello

---
+++ address.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+	printf("41-Tagore Villa\n Chakrata Road\n Dehradun");
+	return 0;
+}
+
---
+++ hello.c
@@ -0,0 +1,11 @@
+/* This is hellow world program
+ * Owner: eshita singh date: 02-09-24
+ * revision history
+ * */
+
+#include<stdio.h>
+
+int main(){
+ printf("Hello World!!");
+ return 1;
+}
